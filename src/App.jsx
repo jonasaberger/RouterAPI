abd1304a8a3508b7a390ff784122d7f523e553bd
AbergerJonas <145124362+jonasaberger@users.noreply.github.com>
2024-05-30 18:43:29 +0200

    Finished Data Formatting

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-bootstrap-range-slider/dist/react-bootstrap-range-slider.css';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
--- src/components/Spotify.jsx
+++ src/components/Spotify.jsx
@@ -3,6 +3,7 @@ import getAccessToken from "../services/getAccessToken";
 import getSpotifyData from "../services/getSpotifyData";
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import RangeSlider from 'react-bootstrap-range-slider'
 
 
 const Spotify = () => {
@@ -18,50 +19,108 @@ const Spotify = () => {
   const [data,setData] = useState(null);
   const [outputData, setOutputData] = useState(null);
 
-//   Handle the entire search operation
+  const [refreshCounter, setRefreshCounter] = useState(0);
+
+  const searchButton = (event) => {
+    event.preventDefault()
+    setRefreshCounter(0)
+    handleSearch()
+  }
+
+  const wrongButton = (event) => {
+    event.preventDefault()
+    setRefreshCounter(refreshCounter+1)
+    console.log(refreshCounter)
+    handleSearch()
+  }
+
+  // Handle the entire search operation
   const handleSearch = async () => {
-    console.log("-- START-SEARCH --")
-    console.log("q = " + searchInput)
-    console.log("category = " + category)
-    fetchAccessToken()
-    fetchData()
-    if(data != null) 
-        formatData()
-    console.log("-- END-SEARCH --")
-  }
+    console.log("-- START-SEARCH --");
+    console.log("q = " + searchInput);
+    console.log("category = " + category);
+    await fetchAccessToken(); // Wait for access token
+    await fetchData(); // Wait for data
+    console.log("-- END-SEARCH --");
+  };
 
-// Format the data so that it can be outputed
-const formatData = () => {
-    if(category === 'album') {
-        setOutputData(
-
-
-        )
-    }
-    else if(category === 'artist') {
-        setOutputData(
-
-        )
-    }
-    else if(category === 'track') {
-        setOutputData(
-
-            
-
-        )
-    }
-}
-
-// Get the Data from the search input
-  const fetchData = async () => {
+  // Get the Data from the search input
+const fetchData = async () => {
     try {
-        const data = await getSpotifyData(searchInput,category,accessToken)
-        setData(data)
-      } catch (error) {
-        console.error('Error fetching data: ', error);
-      }
-  }
-
+      const fetchedData = await getSpotifyData(searchInput, category, accessToken);
+      setData(fetchedData);
+      console.log(fetchedData)
+      formatData(fetchedData); // Call formatData after setting data
+    } catch (error) {
+      console.error('Error fetching data: ', error);
+    }
+  };
+  
+  // Format the data so that it can be outputted
+  const formatData = (data) => {
+    if (category === 'album') {
+      let albumData = data.albums.items[0+refreshCounter]
+      setOutputData(
+        <>
+          <h1 className="AlbumName"><b>{albumData.name}</b></h1>
+          <div>{(albumData.artists).map((artist) => (
+            <h3>{artist.name}</h3>
+          ))}</div>
+          <img className="AlbumImage" src={albumData.images[1].url} alt="Album"/>
+          <Button
+                variant="dark"
+                onClick={openLink(albumData.external_urls.spotify)}
+                disabled={checkedIndex === null || searchInput === ''}>Open Link</Button>
+          <b>{albumData.release_date}</b>
+          <p>Tracks: {albumData.total_tracks} | Type: {albumData.type}</p>
+          
+          
+        </>
+      );
+    } 
+    else if (category === 'artist') {
+      let artistData = data.artists.items[0+refreshCounter];
+      setOutputData(
+        <>
+          <h1 className="ArtistName">{artistData.name}</h1>
+          <img className="ArtistImage" src={artistData.images[1].url} alt="Artist" />
+          <i className="FollowerCount">{formatNumberWithDots(artistData.followers.total)} Followers</i>
+          <ul className="GenreList">{(artistData.genres).map((label,index) => (
+            <li>{label}</li>
+          ))}</ul>
+          <Button
+                variant="dark"
+                onClick={openLink(artistData.external_urls.spotify)}
+                disabled={checkedIndex === null || searchInput === ''}>Open Link</Button>
+          <b>Popularity Score</b>
+          <RangeSlider
+            value={artistData.popularity}
+            variant="dark"
+            />
+        </>
+      );
+    }
+    else if (category === 'track') {
+    let trackData = data.tracks
+      setOutputData(
+        <>
+            <b>Total Results:: {trackData.total}</b>
+            <div>{(trackData.items).map((track) => (
+                <>
+                <h5>
+                    <Button
+                    variant="dark"
+                    onClick={openLink(track.external_urls.spotify)}
+                    disabled={checkedIndex === null || searchInput === ''}> </Button> {track.name} : {formatMsToMinutes(track.duration_ms)}</h5>
+                    <div>{(track.artists).map((artist) => (
+                        <p>- {artist.name}</p>
+                    ))}</div>
+                </>
+            ))}</div>
+        </>
+      );
+    }
+  };
 //   Gets a AccessToken from the Spotify-API Service
   const fetchAccessToken = async () => {
     try {
@@ -71,6 +130,7 @@ const formatData = () => {
       console.error('Error fetching access token:', error);
     }
   };
+
 
 //   Changes the category variable based on the ticked box
   const handleCheckboxChange = async (index) => {
@@ -94,37 +154,83 @@ const formatData = () => {
     console.log('Selected category:', selectedCategory);
   };
 
+
+  const formatNumberWithDots = (number) => {
+    let numberStr = number.toString().split('');
+    let formattedNumber = '';
+    let digitCount = 0;
+  
+    for (let i = numberStr.length - 1; i >= 0; i--) {
+      formattedNumber = numberStr[i] + formattedNumber;
+      digitCount++;
+  
+      if (digitCount % 3 === 0 && i !== 0) {
+        formattedNumber = '.' + formattedNumber;
+      }
+    }
+  
+    return formattedNumber;
+  };
+
+  const formatMsToMinutes = (ms) => {
+    let seconds = ms / 1000;
+    let minutes = Math.floor(seconds / 60);
+    let remainingSeconds = Math.floor(seconds % 60);
+
+    // Add leading zeros if needed
+    let formattedMinutes = (minutes < 10 ? '0' : '') + minutes;
+    let formattedSeconds = (remainingSeconds < 10 ? '0' : '') + remainingSeconds;
+    return formattedMinutes + ':' + formattedSeconds;
+};
+
+  const openLink = (url) => {
+    return (event) => {
+        event.preventDefault();
+
+        // Open the link in a new tab
+        window.open(url);
+    }
+};
   return (
     <div className="SpotifyContainer">
       <h2 className="SpotifyTitle">Spotify</h2>
       <Form>
         <Form.Group controlId="searchInput">
-          <Form.Label>Search</Form.Label>
           <Form.Control
             type="text"
-            placeholder="Enter search query"
+            placeholder="Enter search input"
             value={searchInput}
             onChange={(e) => setSearchInput(e.target.value)}
           />
         </Form.Group>
-        {['Album', 'Artist', 'Track'].map((label, index) => (
-          <Form.Check
-            key={index}
-            type="checkbox"
-            label={label}
-            checked={checkedIndex === index}
-            onChange={() => handleCheckboxChange(index)}
-          />
-        ))}
+        <Form.Group className="SpotifyCheckboxContainer">
+            {['Album', 'Artist', 'Track'].map((label, index) => (
+            <Form.Check
+                key={index}
+                type="checkbox"
+                label={label}
+                checked={checkedIndex === index}
+                onChange={() => handleCheckboxChange(index)}
+            />
+            
+            ))}
+        </Form.Group>
       </Form>
       <Button
         variant="dark"
-        onClick={handleSearch}
+        onClick={searchButton}
         disabled={checkedIndex === null || searchInput === ''}
       >
         Search
       </Button>
       {outputData && <div className="SpotifyDataContainer">{outputData}</div>}
+      <Button
+        variant="light"
+        onClick={wrongButton}
+        disabled={outputData === null || refreshCounter >= 20}
+      >
+        Wrong One?
+      </Button>
     </div>
   );
 };
